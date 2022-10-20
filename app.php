8d3150f93b9b5741f1eb5b06ba0ff812fc6de74f
camCampus <camille.bochar@le-campus-numerique.fr>
2022-10-20 14:05:08 +0200

    projet end

--- app.php
+++ app.php
@@ -14,70 +14,67 @@ use App\Animals\Zebra;
 use App\Caracteristique\Cara;
 use App\Zoo;
 
-//$animals = [
-//    Fish::class => [
-//        "count" => 5,
-//        "basename"=> "Poisson",
-//        "instance" => [],
-//    ],
-//    BubbleFish::class => [
-//    "count" => 3,
-//    "basename"=> "BubbleFish",
-//    "instance" => [],
-//],
-//    CatFish::class => [
-//        "count" => 2,
-//        "basename"=> "CatFish",
-//        "instance" => [],
-//    ],
-//    ClownFish::class => [
-//        "count" => 1,
-//        "basename"=> "ClownFish",
-//        "instance" => [],
-//    ],
-//    Elephant::class => [
-//        "count" => 2,
-//        "basename"=> "Elephant",
-//        "instance" => [],
-//    ],
-//    Zebra::class => [
-//        "count" => 1,
-//        "basename"=> "Zebra",
-//        "instance" => [],
-//    ],
-//    Parrot::class => [
-//        "count" => 10,
-//        "basename"=> "Parrot",
-//        "instance" => [],
-//    ],
-//    Dove::class => [
-//        "count" => 2,
-//        "basename"=> "Dove",
-//        "instance" => [],
-//    ],
-//];
-//
-//foreach ($animals as $index => &$animal){
-//    for ($i = 0; $i < $animal["count"]; $i++) {
-//
-//
-//        /** @var Animal $instance */
-//        $instance = new $index($animal['basename'] . ' ' . ($i + 1));
-//
-//        Zoo::addAnimal($instance);
-////        echo $instance->getName() . PHP_EOL;
-////        echo $instance->noise() . PHP_EOL;
-////
-////        $animal['instance'][] = $instance;
-//    }
-//}
-//
-//Zoo::visitTheZoo();
+$animals = [
+    Fish::class => [
+        "count" => 5,
+        "basename"=> "Poisson",
+        "instance" => [],
+    ],
+    BubbleFish::class => [
+    "count" => 3,
+    "basename"=> "BubbleFish",
+    "instance" => [],
+],
+    CatFish::class => [
+        "count" => 2,
+        "basename"=> "CatFish",
+        "instance" => [],
+    ],
+    ClownFish::class => [
+        "count" => 1,
+        "basename"=> "ClownFish",
+        "instance" => [],
+    ],
+    Elephant::class => [
+        "count" => 2,
+        "basename"=> "Elephant",
+        "instance" => [],
+    ],
+    Zebra::class => [
+        "count" => 1,
+        "basename"=> "Zebra",
+        "instance" => [],
+    ],
+    Parrot::class => [
+        "count" => 10,
+        "basename"=> "Parrot",
+        "instance" => [],
+    ],
+    Dove::class => [
+        "count" => 2,
+        "basename"=> "Dove",
+        "instance" => [],
+    ],
+];
 
-$p = new Fish('bob');
-$cara = new Cara;
-$cara->checkType($p);
-echo $cara;
+foreach ($animals as $index => &$animal){
+    for ($i = 0; $i < $animal["count"]; $i++) {
+
+
+        /** @var Animal $instance */
+        $instance = new $index($animal['basename'] . ' ' . ($i + 1));
+
+        Zoo::addAnimal($instance);
+//        echo $instance->getName() . PHP_EOL;
+//        echo $instance->noise() . PHP_EOL;
+//
+//        $animal['instance'][] = $instance;
+    }
+}
+
+Zoo::visitTheZoo();
+
+
 
 
 
